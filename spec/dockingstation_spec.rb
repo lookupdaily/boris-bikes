bc4d5b4bcc543e7cae901d66cb010d68b6ea1163
Liz Daly <liz7daly@gmail.com>
2020-01-21 16:44:46 +0000

    Add release_bike message

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,7 @@
 require 'dockingstation'
 
 describe DockingStation do 
-
+  it {is_expected.to respond_to(:release_bike)}
+    # station = DockingStation.new
+    # expect(station).to respond_to(:release_bike)
 end
